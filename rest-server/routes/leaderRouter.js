3ce14b8aa1d29e16861701a65f3f6a0833f96525
Zichen Wang <wangzichen530@hotmail.com>
2016-10-02 21:15:35 -0500

    rest api and authentication

---
+++ rest-server/routes/leaderRouter.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Leaderships = require('../models/leadership');
+
+var leaderRouter = express.Router();
+
+leaderRouter.route('/')
+
+.get(function(req,res,next){
+
+    Leaderships.find({}, function (err, dish) {
+      if (err) throw err;
+      res.json(dish);
+    });
+       
+})
+
+.post(function(req, res, next){
+  Leaderships.create(req.body, function (err, leader) {
+        if (err) throw err;
+        console.log('Leader created!');
+        var id = leader._id;
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Added the leader with id: ' + id);
+    });    
+    
+})
+
+.delete(function(req, res, next){
+    Leaderships.remove({}, function (err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
+});
+
+
+leaderRouter.route('/:leaderId')
+
+.get(function(req,res,next){
+  Leaderships.findById(req.params.leaderId, function (err, leader) {
+        if (err) throw err;
+        res.json(leader);
+    });
+       
+})
+
+.put(function(req, res, next){
+  Leaderships.findByIdAndUpdate(req.params.leaderId, {
+    $set: req.body
+  }, {
+    new: true
+  }, function (err, leader) {
+    if (err) throw err;
+    res.json(leader);
+  });
+      
+})
+
+.delete(function(req, res, next){
+  Leaderships.findByIdAndRemove(req.params.leaderId, function (err, resp) {        
+      if (err) throw err;
+        res.json(resp);
+    });
+     
+});
+
+module.exports = leaderRouter;
---
+++ rest-server/routes/promoRouter.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Promotions = require('../models/promotions');
+
+var promoRouter = express.Router();
+promoRouter.route('/')
+
+.get(function(req,res,next){
+    Promotions.find({}, function (err, dish) {
+      if (err) throw err;
+      res.json(dish);
+    });
+})
+
+.post(function(req, res, next){
+  Promotions.create(req.body, function (err, promo) {
+        if (err) throw err;
+        console.log('Promotion created!');
+        var id = promo._id;
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Added the Promotion with id: ' + id);
+    });    
+})
+
+.delete(function(req, res, next){
+  Promotions.remove({}, function (err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
+});
+
+
+promoRouter.route('/:promoId')
+
+
+.get(function(req,res,next){
+  Promotions.findById(req.params.promoId, function (err, promo) {
+        if (err) throw err;
+        res.json(promo);
+    });
+})
+
+.put(function(req, res, next){
+  Promotions.findByIdAndUpdate(req.params.promoId, {
+    $set: req.body
+  }, {
+    new: true
+  }, function (err, promo) {
+    if (err) throw err;
+    res.json(promo);
+  });
+        
+})
+
+.delete(function(req, res, next){
+  Promotions.findByIdAndRemove(req.params.promoId, function (err, resp) {        
+      if (err) throw err;
+        res.json(resp);
+    });
+      
+});
+
+module.exports = promoRouter;
